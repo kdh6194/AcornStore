a2fc6b42e8b536b1ee78944a3af0b93029dc76ba
khd6194 <kdh6194@naver.com>
2023-08-23 15:13:00 +0900

    저장소 초기화

---
+++ src/main/java/com/acorn/acornstore/AcornStoreApplication.java
@@ -0,0 +1,13 @@
+package com.acorn.acornstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcornStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcornStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acorn/acornstore/AcornStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acorn.acornstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcornStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
