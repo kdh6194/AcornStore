c197fce59bc151dff9bb7ba8d1b953553c98c8f7
khd6194 <kdh6194@naver.com>
2023-09-18 20:40:23 +0900

    feature/user - 에러발생 부분 수정

--- src/main/java/com/acorn/acornstore/domain/repository/CartItemRepository.java
+++ src/main/java/com/acorn/acornstore/domain/repository/CartItemRepository.java
@@ -1,7 +1,8 @@
 package com.acorn.acornstore.domain.repository;
 
 import com.acorn.acornstore.domain.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CartItemRepository {
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     void delete(CartItem itemToRemove);
 }
